61dde06b3f185b3f97b9ffb4be3a4c2e0b0b287c
Jiale Yang <43997436+HappyDog-plus@users.noreply.github.com>
2024-11-02 19:59:13 +0800

    WMY topic exercise match (#3)
    * Add files via upload
    
    * Organized the code structure, removed duplicate sections
    
    ---------
    
    Co-authored-by: Mingyu Wei <129271232+YukinoWmy@users.noreply.github.com>

--- client/client.py
+++ client/client.py
@@ -27,21 +27,27 @@ if __name__ == "__main__":
     
 
     # url = "https://4okpi1941999.vicp.fun/course_invoke"
-    url = "http://127.0.0.1:8000/recognize"
+    url = "http://127.0.0.1:8001/model"
 
     # # Test model
+    # Test model conversation
     # img_path = Path("image") / "test_img.jpg"
     # img_base64 = img_encode(img_path)
     # data0 = { "user_id": "userInfo1", "time_span": str(datetime.now()), "mode_code": int(0), "input_text": "Hello! how are you?", "image": ""}
     # response = requests.post(url, json=data0)
     # print(response.json())
 
+    # Test searching exercises based on student input.
+    data = { "user_id": "userInfo1", "time_span": str(datetime.now()), "mode_code": int(2), "input_text": "I want to do some exercise about Glaucoma", "image": ""}
+    response = requests.post(url, json=data)
+    print(response.json())
+
 
     # Test XunFei Recognize API
-    file_path = Path("audio") / "test_wav.wav"
-    with open(file_path, 'rb') as f:
-        response = requests.post(url, files={"file": f})
-    print(response.json())
+    # file_path = Path("audio") / "test_wav.wav"
+    # with open(file_path, 'rb') as f:
+    #     response = requests.post(url, files={"file": f})
+    # print(response.json())
 
 
     # Test in-class exercise judge model.
